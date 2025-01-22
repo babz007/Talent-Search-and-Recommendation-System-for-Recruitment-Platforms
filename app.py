91a48e09d9602de6d5bd64d6178512e2cd0148f3
Adeyemi Aina <ainababs0@gmail.com>
2025-01-22 13:48:47 -0500

    setup information

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Talent Search System"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
